001da94c879fa3afd2392a7f184aa742132d3852
bprivalov <pryvalov.bogddan@gmail.com>
2023-05-12 14:34:17 +0300

    updated Carousel test

--- tests/Carousel.test.tsx
+++ tests/Carousel.test.tsx
@@ -1,16 +1,67 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { fireEvent, render } from '@testing-library/react';
 
 import 'jest-canvas-mock';
 
-import { Carousel } from '../src';
+import { Carousel, CarouselContextProvider } from '../src';
 
-describe('Common render', () => {
-  it('renders without crashing', () => {
-    render(
-      <Carousel i18n={'Cards'} cards={[]}>
-        <div>Card description</div>
+const mockCards = [
+  { key: '1', title: 'Card 1' },
+  { key: '2', title: 'Card 2' },
+  { key: '3', title: 'Card 3' },
+];
+
+const Card = (props: any) => <div>{props.title}</div>;
+describe('Carousel test render', () => {
+  it('snapshot Carousel', () => {
+    const { baseElement } = render(
+      <Carousel i18n={'Cards'} cards={mockCards} defaultCardsCount={1}>
+        <Card />
       </Carousel>,
     );
+
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  test('renders carousel container', () => {
+    const { getByTestId } = render(
+      <Carousel i18n={'Cards'} cards={mockCards} defaultCardsCount={1}>
+        <Card />
+      </Carousel>,
+    );
+    const carouselContainer = getByTestId('carousel-Cards');
+
+    expect(carouselContainer).toBeInTheDocument();
+  });
+
+  it('handles next page click', () => {
+    const { getByTestId } = render(
+      <CarouselContextProvider>
+        <Carousel i18n={'Cards'} cards={mockCards} defaultCardsCount={1}>
+          <Card />
+        </Carousel>
+      </CarouselContextProvider>,
+    );
+    const nextButton = getByTestId('pagination-Button3');
+    fireEvent.click(nextButton);
+
+    const activePageButton = getByTestId('pagination-Button3');
+    expect(activePageButton).toHaveStyle('background-color: hsla(200, 85%, 60%, 0.8)');
+  });
+
+  it('handles previous page click', () => {
+    const { getByTestId } = render(
+      <CarouselContextProvider>
+        <Carousel i18n={'Cards'} cards={mockCards} defaultCardsCount={1} defaultActivePage={2}>
+          <Card />
+        </Carousel>
+      </CarouselContextProvider>,
+    );
+    const previousButton = getByTestId('pagination-Button1');
+    fireEvent.click(previousButton);
+
+    const activePageButton = getByTestId('pagination-Button1');
+    expect(activePageButton).toHaveStyle('background-color: hsla(200, 85%, 60%, 0.8)');
   });
 });
---
+++ tests/useCarouselContext.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { CarouselContextProvider, useCarouselContext } from '../src';
+
+// Helper function to render components with the CarouselContextProvider
+const renderWithContext = (component: React.ReactNode) => {
+  return render(<CarouselContextProvider>{component}</CarouselContextProvider>);
+};
+
+test('should update current page when next page is clicked', () => {
+  // Render a component that uses the CarouselContext
+  const TestComponent = () => {
+    const { currentPage, handleNextPage } = useCarouselContext();
+
+    return (
+      <div>
+        <span data-testid='currentPage'>{currentPage}</span>
+        <button data-testid='nextButton' onClick={handleNextPage}>
+          Next
+        </button>
+      </div>
+    );
+  };
+
+  const { getByTestId } = renderWithContext(<TestComponent />);
+
+  // Verify the initial state
+  const currentPageElement = getByTestId('currentPage');
+  expect(currentPageElement.textContent).toBe('1');
+
+  // Simulate clicking the next button
+  const nextButton = getByTestId('nextButton');
+  fireEvent.click(nextButton);
+
+  // Verify the updated state
+  expect(currentPageElement.textContent).toBe('2');
+});
